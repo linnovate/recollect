5d3a1a6063a1ef87dac4fb3c532bf5ca65f24fcc
Orit Persik <orit@linnovate.net>
2017-11-14 14:28:53 +0200

    update request data

--- src/consumer/webhook.js
+++ src/consumer/webhook.js
@@ -8,20 +8,21 @@ const BASE_QUEUE_NAME = process.env.BASE_QUEUE_NAME;
 const start = () => {
   consume(`${BASE_QUEUE_NAME}-webhook`, (msg, error, done) => {
     console.log('message from webhook queue', msg);
-    return done();
-    // const options = {
-    //   uri: msg.webhookUrl,
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    //   form: msg,
-    // };
+    const options = {
+      uri: msg.webhookUrl,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: msg,
+    };
 
-    // request(options, (err, response, body) => {
-    //   if (!err && response.statusCode >= 200 && response.statusCode < 300 && body.length) return done();
-    //   return error();
-    // });
+    request(options, (err, response, body) => {
+      console.log(err, body);
+      return done();
+      // if (!err && response.statusCode >= 200 && response.statusCode < 300 && body.length) return done();
+      // return error();
+    });
   });
 };
 
