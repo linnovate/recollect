5faef9142adf4c2e4ec2d6569580e1894de9cc09
yitzchak <yitzchak@linnovate.net>
2019-01-03 13:21:18 +0200

    changing providers elastic to connect with an array of hosts instead of a single one to support direct using of es cluster, with backwards compitability.

--- src/providers/es.js
+++ src/providers/es.js
@@ -2,9 +2,22 @@ const elasticsearch = require('elasticsearch');
 
 const ES_URL = process.env.ES_URL;
 const ES_INDEX = process.env.ES_INDEX;
+const hosts = [ 
+        process.env.ES_URL,
+        process.env.ES_URL_NODE_1,
+        process.env.ES_URL_NODE_2,
+        process.env.ES_URL_NODE_3
+     ];
+
+for(let i in hosts){
+  if(hosts[i] == null || hosts[i] === '')
+    hosts.splice(i, 1);
+}
+
+console.log(hosts);
 
 const client = new elasticsearch.Client({
-  host: ES_URL,
+  hosts: hosts
 });
 
 const index = (doc, cb) => {
